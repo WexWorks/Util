44e96be41e0241db30283528fe5d7d969d686bbe
Dan Wexler <wex@the11ers.com>
2012-01-25 10:21:01 -0800

    Initial Commit

---
+++ Util/Util.h
@@ -0,0 +1,13 @@
+//
+//  Util.h
+//  Util
+//
+//  Created by Daniel Wexler on 1/25/12.
+//  Copyright (c) 2012 The 11ers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Util : NSObject
+
+@end
---
+++ Util/Util.m
@@ -0,0 +1,23 @@
+//
+//  Util.m
+//  Util
+//
+//  Created by Daniel Wexler on 1/25/12.
+//  Copyright (c) 2012 The 11ers. All rights reserved.
+//
+
+#import "Util.h"
+
+@implementation Util
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
