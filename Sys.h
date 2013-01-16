4b4b2a233367edb3d1b32786f1fae1b2a005d4c1
Dan Wexler <wex@the11ers.com>
2013-01-16 12:18:21 -0800

    Refactor image loading API to split up metadata from pixel data.

--- Sys.h
+++ Sys.h
@@ -3,7 +3,9 @@
 #ifndef SYS_H
 #define SYS_H
 
-#include <stdlib.h>
+#include <map>
+#include <string>
+#include <vector>
 
 
 namespace tui { class Event; }
@@ -19,16 +21,51 @@ public:
   Callbacks() {}
   virtual ~Callbacks() {}
   
+  // Returns a URL to a file within the app's resource bundle
   virtual bool FindResourcePath(const char *name, char path[1024]) = 0;
+  
+  // Returns a URL to a file in the app's local documents folder
   virtual bool FindAppCachePath(const char *name, char path[1024]) = 0;
+  
+  // Load an image from the app's resource bundle (blocking)
   virtual bool LoadImageResource(const char *name, size_t *w, size_t *h,
                                  size_t *channelCount,
                                  const unsigned char **texel) = 0;
+  
+  // Load a text file from the app's resource bundle (blocking)
   virtual bool LoadText(const char *name, const char **text) = 0;
-  virtual bool LoadImageDirectory(const char *name) = 0;
-  virtual bool LoadImage(const char *name, void *data) = 0;
+  
+  // Load a directory of albums (folders) and images (asynchronous)
+  virtual bool LoadImageDirectory(const char *url) = 0;
+  
+  // Load only the metadata, not the pixels, from an image (asynchronous)
+  virtual bool LoadImageMetadata(const char *url) = 0;
+  
+  // Load the full image of pixels into memory (asynchronous)
+  virtual bool LoadImagePixels(const char *url) = 0;
 
+  // Return the "scaling factor" for this display (poorly defined!)
   virtual float PixelScale() const = 0;
+};
+
+
+// Image metadata, broken into sections, each with a title and key-value map
+struct MetadataSection {
+  std::string title;
+  std::map<std::string, std::string> keyValueMap;
+};
+
+struct Metadata {
+  ~Metadata() { for (size_t i=0; i<sectionVec.size();++i) delete sectionVec[i];}
+  void Copy(const Metadata &src) {
+    for (size_t i = 0; i < src.sectionVec.size(); ++i) {
+      MetadataSection *s = new MetadataSection;
+      *s = *src.sectionVec[i];
+      sectionVec.push_back(s);
+    }
+  }
+  
+  std::vector<const MetadataSection *> sectionVec;
 };
 
 
@@ -50,18 +87,22 @@ public:
   // Called at startup and whenever the device orientation changes
   virtual bool SetDeviceResolution(int w, int h) = 0;
   
-  // Called by Callbacks::LoadImageDirectory
-  virtual bool AddImage(const char *album, const char *name, const char *url,
-                        size_t w, size_t h, const unsigned char *thumb) = 0;
+  // Called asynchronously by Callbacks::LoadImageDirectory
+  virtual bool AddImage(const char *album, const char *name, const char *url)=0;
   virtual bool AddAlbum(const char *name, const char *url, size_t w, size_t h,
                         const unsigned char *thumb) = 0;
 
-  // Called by Callbacks::LoadImage
-  virtual bool LoadImage(const char *name, void *data, size_t w, size_t h,
-                         size_t channelCount, size_t bytePerChannelCount,
-                         const unsigned char *pixel,
-                         size_t metadataSectionCount, const char **title,
-                         const char **key, const char **value) = 0;
+  // Called asynchronously by Callbacks::LoadImageMetadata
+  virtual bool SetImageThumbnail(const char *url, size_t w, size_t h,
+                                 const unsigned char *thumb) = 0;
+  
+  // Called asynchronously by Callbacks::LoadImageMetadata
+  virtual bool SetImageMetadata(const char *url, const Metadata &metadata) = 0;
+  
+  // Called asynchronously by Callbacks::LoadImagePixels
+  virtual bool SetImagePixels(const char *url, size_t w, size_t h,
+                              size_t channelCount, size_t bytesPerChannel,
+                              const unsigned char *pixel) = 0;
   
 protected:
   App() {}                                            // Derived class factory
