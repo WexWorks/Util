8193e5f94b52b9f01cf3ff9190af262373c85f7b
Gilles Dezeustre <gilles@the11ers.com>
2013-08-14 20:09:47 -0700

    Pretty good checkpoint optimizing rendering.

--- TriStrip.h
+++ TriStrip.h
@@ -18,47 +18,52 @@
 
 class TriStrip {
 public:
-  TriStrip() : mFlags(0) {}
+  TriStrip() : mVertexCount(0), mIndexCount(0), mFlags(0) {}
   ~TriStrip() {}
   enum Flags { ATTR_0_FLAG=1, ATTR_1_FLAG=2, ATTR_2_FLAG=4, MATERIAL_FLAG=8 };
   void Init(size_t vertexCount, size_t indexCount, unsigned long flags);
-  void InitDisc(const Imath::V3f &center, const Imath::V3f &N, float radius,
-                size_t vertexCount, unsigned long flags, int uvAttrIdx);
-  void InitExtrusion(size_t vertexCount, const Imath::V3f *face,
-                     size_t segmentCount, const Imath::V3f *scale,
-                     unsigned long attrNFlag);
-  void InitBox(const Imath::V3f &min, const Imath::V3f &max,
-               unsigned long attrNFlag, unsigned long attrUVFlag);
+  void VertexResize(size_t newSize);
+  void IndexResize(size_t newSize);
   void InitTransform(const TriStrip &src, const Imath::M44f &T);
   void Clear();
-  void Reserve(size_t vertexCount, size_t indexCount);
   bool Append(const TriStrip &tristrip);
   void ToLines(std::vector<unsigned short> &lineIdx) const;
   
   // Mutable access
-  Imath::V3f &P(size_t i) {  assert(i < mP.size()); return mP[i]; }
+  Imath::V3f &P(size_t i) {  assert(i < mVertexCount); return mP[i]; }
   Imath::V4f &Attr(size_t a, size_t v) {
-    assert(a < kMaxAttr); assert(v < mA[a].size()); return mA[a][v]; }
-  unsigned short &Idx(size_t i) { assert(i < mIdx.size()); return mIdx[i]; }
+    assert(a < kMaxAttr); assert(v < mVertexCount); return mA[a][v]; }
+  unsigned short &Idx(size_t i) { assert(i < mIndexCount); return mIdx[i]; }
   unsigned short &Material(size_t i) {
-    assert(i < mMaterial.size()); return mMaterial[i]; }
+    assert(i < mVertexCount); return mMaterial[i]; }
   
   // Constant access
-  size_t VertexCount() const { return mP.size(); }
-  size_t IndexCount() const { return mIdx.size(); }
-  bool Empty() const { return mIdx.size() == 0; }
-  const Imath::V3f &P(size_t i) const { assert(i < mP.size()); return mP[i]; }
+  size_t VertexCount() const { return mVertexCount; }
+  size_t IndexCount() const { return mIndexCount; }
+  bool VertexEmpty() const { return VertexCount() == 0; }
+  bool IndexEmpty() const { return IndexCount() == 0; }
+  const Imath::V3f &P(size_t i) const { assert(i < mVertexCount); return mP[i]; }
   const Imath::V4f &Attr(size_t a, size_t v) const {
-    assert(a < kMaxAttr); assert(v < mA[a].size()); return mA[a][v]; }
+    assert(a < kMaxAttr); assert(v < mVertexCount); return mA[a][v]; }
   bool AttrEnabled(size_t i) const {
     assert(i < kMaxAttr); return mFlags & (1L << i); }
   const unsigned short &Idx(size_t i) const {
-    assert(i < mIdx.size()); return mIdx[i]; }
+    assert(i < mIndexCount); return mIdx[i]; }
   const unsigned short &Material(size_t i) const {
-    assert(i < mMaterial.size());return mMaterial[i]; }
+    assert(i < mVertexCount);return mMaterial[i]; }
   
 private:
-  static const size_t kMaxAttr = 3;  
+  
+  void SetVertexCapacity(size_t newCapacity);  
+  size_t VertexCapacity() const;
+  size_t MaxCapacity() const;
+  size_t IndexCapacity() const;
+  void SetIndexCapacity(size_t newCapacity);  
+  
+  static const size_t kMaxAttr = 3;
+  
+  size_t mVertexCount;
+  size_t mIndexCount;
   
   unsigned long mFlags;
   std::vector<Imath::V3f> mP;
